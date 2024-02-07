beda644f8b909c3c0db109ee21f25f19c124bd22
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-02-07 20:29:18 +0100

    Fix srad_v2

--- cuda/srad/srad_v2/srad.h
+++ cuda/srad/srad_v2/srad.h
@@ -1,15 +1,16 @@
 #define STR_SIZE 256
 
 #ifdef RD_WG_SIZE_0_0
-        #define BLOCK_SIZE RD_WG_SIZE_0_0
+#define BLOCK_SIZE RD_WG_SIZE_0_0
 #elif defined(RD_WG_SIZE_0)
-        #define BLOCK_SIZE RD_WG_SIZE_0
+#define BLOCK_SIZE RD_WG_SIZE_0
 #elif defined(RD_WG_SIZE)
-        #define BLOCK_SIZE RD_WG_SIZE
+#define BLOCK_SIZE RD_WG_SIZE
 #else
-        #define BLOCK_SIZE 16
+#define BLOCK_SIZE 16
 #endif
 
 #define GPU
 // #define TIMER
-#define OUTPUT
+#define COMPARE // Compare values in results.txt with calculated results.
+// #define OUTPUT  // Print output to stdout, so that you can store it in results.txt
