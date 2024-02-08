aa61e1a36ba9e722534edebb29aa334196707652
Dimitris Papagiannis <d.papag.a@gmail.com>
2024-02-08 23:03:46 +0100

    Update benchmarks

--- data/gaussian/matrixGenerator.py
+++ data/gaussian/matrixGenerator.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 # This script generates matrices to solve a set of equations with n variables and n unknowns.
 # For each iteration, an n x n matrix and a 1 x n vector are created to describe the set of
@@ -15,9 +15,9 @@
 # the n x n coefficient matrix, the 1 x n vector, and the 1 x n solution.
 # Each output file has the name matrixN.txt, where N is the value of n (e.g., matrix4.txt)
 #
-# The n x n matrix values and solution vector are confined to the values -0.9 to 0.9 
+# The n x n matrix values and solution vector are confined to the values -0.9 to 0.9
 # (one decimal place), but the 1 x n vector can have two decimal place values.
-# 
+#
 # usage:
 # ./matrixGenerator a b c
 #
@@ -43,53 +43,53 @@ c = 1
 # parse command line
 try:
     a = int(sys.argv[1])
-    b = a+1
-    b = int(sys.argv[2])+1
+    b = a + 1
+    b = int(sys.argv[2]) + 1
     c = int(sys.argv[3])
 except IndexError:
     pass
 except ValueError:
     pass
 
-for sqSize in range(a,b,c):
-	#size = 100
-	#size = sqSize*sqSize
-	size = sqSize;
-	print size
-	solnVec = []
-	matrix = []
-	bVector = []
-	
-	filename = "matrix"+str(size)+".txt"
-	#f = open("matrix100.txt",'w')
-	f = open(filename,'w')
-	f.write(str(size)+"\n\n")
-	for i in range(size):
-		#solnVec.append(random.randint(-size,size)/float(size))
-		solnVec.append(random.randint(-10,10)/float(10))
-	
-	for i in range(size):
-		matrixRow = []
-		for j in range(size):
-			#matrixRow.append(random.randint(-size,size)/float(size))
-			matrixRow.append(random.randint(-10,10)/float(10))
-		matrix.append(matrixRow)
-	
-	for i in matrix:
-		linResult = 0
-		for j in range(size):
-			f.write(str(i[j])+"\t")
-			linResult+=i[j]*solnVec[j]
-		bVector.append(linResult)
-		f.write("\n")
-	
-	f.write("\n")
-	for i in bVector:
-		f.write(str(i)+"\t")
-	
-	f.write("\n\n");
-	for i in solnVec:
-		f.write(str(i)+"\t")
-	f.write("\n\n")
-	
-	f.close()
+for sqSize in range(a, b, c):
+    # size = 100
+    # size = sqSize*sqSize
+    size = sqSize
+    print(size)
+    solnVec = []
+    matrix = []
+    bVector = []
+
+    filename = "matrix" + str(size) + ".txt"
+    # f = open("matrix100.txt",'w')
+    f = open(filename, "w")
+    f.write(str(size) + "\n\n")
+    for i in range(size):
+        # solnVec.append(random.randint(-size,size)/float(size))
+        solnVec.append(random.randint(-10, 10) / float(10))
+
+    for i in range(size):
+        matrixRow = []
+        for j in range(size):
+            # matrixRow.append(random.randint(-size,size)/float(size))
+            matrixRow.append(random.randint(-10, 10) / float(10))
+        matrix.append(matrixRow)
+
+    for i in matrix:
+        linResult = 0
+        for j in range(size):
+            f.write(str(i[j]) + "\t")
+            linResult += i[j] * solnVec[j]
+        bVector.append(linResult)
+        f.write("\n")
+
+    f.write("\n")
+    for i in bVector:
+        f.write(str(i) + "\t")
+
+    f.write("\n\n")
+    for i in solnVec:
+        f.write(str(i) + "\t")
+    f.write("\n\n")
+
+    f.close()
